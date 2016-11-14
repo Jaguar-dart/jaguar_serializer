cdabb4c0df0f36f5bf3948cd77c99be1339dbd5d
Ravi Teja Gudapati <tejainece@gmail.com>
2016-11-14 18:43:34 +0100

    Removed unused imports. Made generator reusable

--- lib/generator/parser/serializer_parser/serializer_parser.dart
+++ lib/generator/parser/serializer_parser/serializer_parser.dart
@@ -2,7 +2,6 @@ library jaguar_serializer.generator.parser.serializer;
 
 import 'dart:mirrors' show MirrorSystem;
 import 'package:analyzer/dart/element/element.dart';
-import 'package:analyzer/dart/element/type.dart';
 import 'package:analyzer/dart/constant/value.dart';
 
 import 'package:source_gen_help/import.dart';
