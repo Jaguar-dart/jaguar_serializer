ce3973923929693bab0beea5770260785122c0e3
Ravi Teja Gudapati <tejainece@gmail.com>
2016-12-28 19:11:46 +0100

    Added JsonSerializable

--- lib/src/json/json.dart
+++ lib/src/json/json.dart
@@ -3,7 +3,7 @@ library serializer.json;
 import 'dart:convert';
 import 'package:jaguar_serializer/src/serializer/import.dart';
 
-abstract class Jsonable<ModelType> {
+abstract class JsonSerializer<ModelType> {
   String toJson(ModelType model);
 
   ModelType fromJson(String json, {ModelType model}) {
@@ -19,9 +19,19 @@ abstract class Jsonable<ModelType> {
 
 /// Mixin that provides encoding and decoding JSON on top of [MapSerializer]
 abstract class JsonMixin<ModelType>
-    implements MapSerializer<ModelType>, Jsonable<ModelType> {
+    implements MapSerializer<ModelType>, JsonSerializer<ModelType> {
   String toJson(ModelType model) => JSON.encode(toMap(model));
 
   ModelType fromJson(String json, {ModelType model}) =>
       fromMap(JSON.decode(json), model: model);
+}
+
+abstract class JsonSerializable<ModelType> {
+  JsonSerializer<ModelType> get jsonSerializer;
+
+  String toJson() => jsonSerializer.toJson(this as ModelType);
+
+  void fromJson(String json) {
+    jsonSerializer.fromJson(json, model: (this as ModelType));
+  }
 }
