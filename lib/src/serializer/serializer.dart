882d1beeaefc0def1f88343e5a28254ae748aeee
Hadrien Lejard <hadrien.lejard@gmail.com>
2017-02-22 18:33:52 +0100

    remove MapSerializer

--- lib/src/serializer/serializer.dart
+++ lib/src/serializer/serializer.dart
@@ -1,84 +1,45 @@
 library jaguar_serializer.serializer;
 
 part 'custom_codec.dart';
-part 'map_serializer.dart';
 part 'map_maker.dart';
 part 'repo.dart';
 
-abstract class Serializer<ModelType> implements MapSerializer<ModelType> {
-  dynamic to(dynamic model) {
+abstract class Serializer<ModelType> {
+  dynamic to(dynamic model, {bool withTypeInfo: false, String typeInfoKey: defaultTypeInfoKey}) {
     if (model is ModelType) {
-      return toMap(model);
+      return toMap(model, withTypeInfo: withTypeInfo, typeInfoKey: typeInfoKey);
     } else if (model is List<ModelType>) {
-      return model.map((ModelType model) => toMap(model)).toList();
+      return model.map((ModelType model) => toMap(model, withTypeInfo: withTypeInfo, typeInfoKey: typeInfoKey)).toList();
     } else {
       throw new Exception("Unknown object type received!");
     }
   }
 
-  dynamic from(dynamic object) {
+  dynamic from(dynamic object, {Type type, String useTypeInfoKey}) {
     if (object is Map) {
-      return fromMap(object);
+      return fromMap(object, typeInfoKey: useTypeInfoKey);
     } else if (object is List<Map>) {
-      return object.map((Map map) => fromMap(map)).toList();
+      return object.map((Map map) => fromMap(map, typeInfoKey: useTypeInfoKey)).toList();
     } else {
       throw new Exception("Unknown object type received!");
     }
   }
 
-  Object fromList(List objects, {Type type, bool useTypeInfo: true}) => objects
-      .map((object) => fromObject(object, type: type, useTypeInfo: useTypeInfo))
-      .toList();
+  /// Encodes model to [Map]
+  Map toMap(ModelType model, {bool withTypeInfo: false, String typeInfoKey});
 
-  Object fromMap(Map map, {Type type, bool useTypeInfo: true, dynamic model}) {
-    MapSerializer serializer;
-    try {
-      serializer = getMapSerializerForType(type) ??
-          SerializerRepo.getMapSerializerForType(type);
-    } catch (e) {
-      if (useTypeInfo == false) {
-        rethrow;
-      }
-      serializer = getMapSerializerFromMap(map) ??
-          SerializerRepo.getMapSerializerFromMap(map);
+  /// Decodes model from [Map]
+  ModelType fromMap(Map map, {ModelType model, String typeInfoKey}) {
+    if (model is! ModelType) {
+      model = createModel();
     }
-    return serializer.fromMap(map, model: model);
+
+    return model;
   }
 
-  MapSerializer getMapSerializerFromMap(Map map) {
-    if (map.containsKey(SerializerRepo.typeInfoKey) &&
-        _mapperString.containsKey(map[SerializerRepo.typeInfoKey])) {
-      return _mapperString[map[SerializerRepo.typeInfoKey]];
-    }
-    return null;
-  }
+  Type get modelType => ModelType;
 
-  MapSerializer getMapSerializerForType(Type type) {
-    if (_mapperType.containsKey(type)) {
-      return _mapperType[type];
-    }
-    return null;
-  }
+  String get modelString => "$ModelType";
 
-  void addSerializer(MapSerializer serializer) {
-    if (!_mapperType.containsKey(serializer.modelType)) {
-      _mapperType[serializer.modelType] = serializer;
-    }
-    if (!_mapperString.containsKey(serializer.modelString)) {
-      _mapperString[serializer.modelString] = serializer;
-    }
-  }
-
-  String encode(dynamic object, {bool withTypeInfo: false}) =>
-      dataEncode(toObject(object, withTypeInfo: withTypeInfo));
-
-  dynamic decode(String data,
-          {Type type, bool useTypeInfo: true, dynamic model}) =>
-      fromObject(dataDecode(data),
-          type: type, useTypeInfo: useTypeInfo, model: model);
-
-  //TODO: method to decode List (JSON, YAML ...) to Stream
-
-  String dataEncode(dynamic object);
-  dynamic dataDecode(String data);
+  ModelType createModel();
 }
